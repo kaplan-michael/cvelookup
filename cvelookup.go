45c574e7fd8d8466d53e2663ec06ea2417c26695
Michael Kaplan <mkaplan@redhat.com>
2020-11-14 21:09:10 +0100

    support multiple CVE as arguments

--- cvelookup.go
+++ cvelookup.go
@@ -1,33 +1,16 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
 	"os"
-	"time"
 )
 
-var httpClient = &http.Client{Timeout: 10 * time.Second}
-
-func getJSON(cve string, target interface{}) error {
-	url := "https://services.nvd.nist.gov/rest/json/cve/1.0/"
-	cveurl := url + cve
-	fmt.Println(cveurl)
-	r, err := httpClient.Get(cveurl)
-	if err != nil {
-		return err
-	}
-	defer r.Body.Close()
-
-	return json.NewDecoder(r.Body).Decode(target)
-}
-
 func main() {
-	cveResponse := new(cveResponseTemplate)
-	cve := os.Args[1]
-	getJSON(cve, cveResponse)
+	for i := 1; i <= len(os.Args[1:]); i++ {
+		cve := os.Args[i]
+		getJSON(cve, cveResponse)
+		printInfo()
+		cveResponse = new(cveResponseTemplate)
 
-	fmt.Println(cveResponse.Result.CVEItems)
+	}
 
 }
